1ed87421b23ac1cda617632e4faec918b483d87a
dangertater <106702944+dangertater@users.noreply.github.com>
2022-12-26 09:56:53 -0700

    fuckzorzing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-	return <>asdf</>
-}
+import { Route, Routes, Link } from "react-router-dom"
+import TestPage1 from "./routes/TestPage1"
+import TestPage2 from "./routes/TestPage2"
 
-export default App
+export default function App() {
+	return (
+		<>
+			<nav>
+				<ul>
+					<li>
+						<Link to="./routes/TestPage1.js" />
+						testPage1
+					</li>
+					<li>
+						<Link to="./routes/TestPage2.js" />
+						testPage2
+					</li>
+				</ul>
+			</nav>
+			<Routes>
+				<Route path="/TestPage1" element={<TestPage1 />}></Route>
+				<Route path="/TestPage2" element={<TestPage2 />}></Route>
+			</Routes>
+		</>
+	)
+}
